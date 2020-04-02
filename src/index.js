b786c05609efc9317f8565efb46f7007a3aab4bd
Mai <anna-maija.rauramaa@aalto.fi>
2020-04-02 17:07:26 +0300

    Three.js installed, React-Globe installed, minor tweaks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,93 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import ReactGlobe from 'react-globe';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import defaultMarkers from './markers';
 
+function getTooltipContent(marker) {
+  return `CITY: ${marker.city} (Value: ${marker.value})`;
+}
+
+//value used to be: Math.floor(Math.random() * 100)
+function App() {
+  const randomMarkers = defaultMarkers.map(marker => ({
+    ...marker,
+    value: marker.value,
+  }));
+  const [markers, setMarkers] = useState([]);
+  const [event, setEvent] = useState(null);
+  const [details, setDetails] = useState(null);
+
+  console.log('markers are: ', markers)
+
+  function onClickMarker(marker, markerObject, event) {
+    setEvent({
+      type: 'CLICK',
+      marker,
+      markerObjectID: markerObject.uuid,
+      pointerEventPosition: { x: event.clientX, y: event.clientY },
+    });
+    setDetails(getTooltipContent(marker));
+  }
+  function onDefocus(previousCoordinates, event) {
+    setEvent({
+      type: 'DEFOCUS',
+      previousCoordinates,
+      pointerEventPosition: { x: event.clientX, y: event.clientY },
+    });
+    setDetails(null);
+  }
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', top: '0px', lef: '0px', margin: '0px' }}>
+      <ReactGlobe
+        markers={markers}
+        markerOptions={{
+          getTooltipContent,
+        }}
+        onClickMarker={onClickMarker}
+        onDefocus={onDefocus}
+        globeOptions={{
+          texture:
+            'https://raw.githubusercontent.com/chrisrzhou/react-globe/master/textures/globe_dark.jpg', //This skin is cooll, but seems to slow down the broswer.
+        }}
+      />
+      {details && (
+        <div
+          style={{ //These are marker's notes' properties.
+            background: '#14141a',
+            position: 'absolute',
+            fontSize: 20,
+            top: 0,
+            right: 0,
+            padding: 12,
+          }}>
+          <p>{details}</p>
+          <p>
+            EVENT: type={event.type}, position=
+            {JSON.stringify(event.pointerEventPosition)})
+          </p>
+        </div>
+      )}
+      <button onClick={() => setMarkers(randomMarkers)}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        Randomize markers
+      </button>
+      <button disabled={markers.length === 0} onClick={() => setMarkers([])}>
+        Clear markers
+      </button>
+      <button
+        disabled={markers.length === randomMarkers.length}
+        onClick={() => setMarkers([...markers, randomMarkers[markers.length]])}>
+        Add marker
+      </button>
+      <button
+        disabled={markers.length === 0}
+        onClick={() => setMarkers(markers.slice(0, markers.length - 1))}>
+        Remove marker
+      </button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
---
+++ src/markers.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: 1,
+    city: 'Singapore',
+    color: 'red',
+    coordinates: [1.3521, 103.8198],
+    value: 50,
+  },
+  {
+    id: 2,
+    city: 'New York',
+    color: 'blue',
+    coordinates: [40.73061, -73.935242],
+    value: 25,
+  },
+  {
+    id: 3,
+    city: 'San Francisco',
+    color: 'orange',
+    coordinates: [37.773972, -122.431297],
+    value: 35,
+  },
+  {
+    id: 4,
+    city: 'Beijing',
+    color: 'gold',
+    coordinates: [39.9042, 116.4074],
+    value: 0,
+  },
+  {
+    id: 5,
+    city: 'Milan',
+    color: 'green',
+    coordinates: [45.464664, 9.188540],
+    value: 400,
+  },
+  {
+    id: 6,
+    city: 'Helsinki',
+    color: 'green',
+    coordinates: [60.192059, 24.94583],
+    value: 80,
+  },
+  {
+    id: 7,
+    city: 'London',
+    color: 'green',
+    coordinates: [51.5074, 0.1278],
+    value: 80,
+  },
+];
