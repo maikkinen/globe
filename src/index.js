c5b908cda188a5246f276e972bf3d4fac1d7ce12
Mai <anna-maija.rauramaa@aalto.fi>
2020-04-06 13:22:38 +0300

    the lightweight updated, supporting different results for different btns

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import ReactGlobe from 'react-globe';
+import ReactGlobe, { defaultMarkerOptions } from 'react-globe';
 
-import { HelsinkiMarkers } from './markers';
+import { AllMarkersByCountry } from './markers';
 
 function getTooltipContent(marker) {
-  return `CITY: ${marker.city} (Value: ${marker.value})`;
+  return (
+    <div>
+      <p>UK: {marker.headline1}</p>
+      <p>FIN: {marker.headline2}</p>
+      <p>SE: {marker.headline3}</p>
+      <p>CH: {marker.headline4}</p>
+      <p>CH: {marker.headline5}</p>
+    </div>
+  )
 }
 
 //value used to be: Math.floor(Math.random() * 100)
 function App() {
-  const initCountryMarkers = HelsinkiMarkers.markers.map(marker => ({
-    ...marker,
-    value: marker.value,
-  }));
+
+  const [country1, setCountry1] = useState('finland')
+  const [country2, setCountry2] = useState("UK")
+
+  const initCountryMarkers = (country) => {
+    const countryMarkers = AllMarkersByCountry[country].markers.map(marker => ({
+      ...marker,
+      value: marker.value,
+    })
+    )
+    console.log("countrymarkers :", countryMarkers)
+    return countryMarkers
+  }
+
   const [markers, setMarkers] = useState([]);
   const [event, setEvent] = useState(null);
   const [details, setDetails] = useState(null);
-
-  console.log('markers are: ', markers)
 
   function onClickMarker(marker, markerObject, event) {
     setEvent({
@@ -39,43 +55,69 @@ function App() {
   }
 
   return (
-    <div style={{ width: '100vw', height: '100vh', top: '0px', lef: '0px', margin: '0px' }}>
+    <div style={{ width: '75vw', height: '75vh', top: '0px', lef: '0px', margin: '0px' }}>
       <ReactGlobe
         markers={markers}
         markerOptions={{
-          getTooltipContent,
+          
+          activeScale: 1.1,
         }}
         onClickMarker={onClickMarker}
         onDefocus={onDefocus}
-        globeOptions={{
-          texture:
-            'https://raw.githubusercontent.com/chrisrzhou/react-globe/master/textures/globe_dark.jpg', //This skin is cooll, but seems to slow down the broswer.
-        }}
+
       />
       {details && (
         <div
           style={{ //These are marker's notes' properties.
-            background: '#14141a',
+            background: 'yellow',
             position: 'absolute',
             fontSize: 20,
-            top: 0,
-            right: 0,
+            top: '10vh',
+            right: '10vh',
             padding: 12,
+            
           }}>
+          
           <p>{details}</p>
           <p>
-            EVENT: type={event.type}, position=
-            {JSON.stringify(event.pointerEventPosition)})
+            California, US
           </p>
         </div>
       )}
-      <button onClick={() => setMarkers(initCountryMarkers)}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
-        London markers
+      <div>
+      <h2>Some deep text that also makes an intro to the different subsets of COVID-19 news.</h2>
+      <button onClick={() => setMarkers(initCountryMarkers('uk'))}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        News on Government
       </button>
-      <button onClick={() => setMarkers(initCountryMarkers)}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
-        Helsinki markers
+      <button onClick={() => setMarkers(initCountryMarkers('finland'))}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        News on Economy
       </button>
-      <button disabled={markers.length === 0} onClick={() => setMarkers([])}>
+      <button onClick={() => setMarkers(initCountryMarkers('finland'))}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        News on Politics
+      </button>
+      <button onClick={() => setMarkers(initCountryMarkers('finland'))}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        News on People
+      </button>
+      <button onClick={() => setMarkers(initCountryMarkers('finland'))}> {/*This thing actually initializes the markers. Just change the wording to be proper. */}
+        News on Healthcare
+      </button>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+
+/*
+
+ globeOptions={{
+          texture:
+            'https://raw.githubusercontent.com/chrisrzhou/react-globe/master/textures/globe_dark.jpg', //This skin is cooll, but seems to slow down the broswer.
+        }}
+
+        <button disabled={markers.length === 0} onClick={() => setMarkers([])}>
         Clear markers
       </button>
       <button
@@ -88,9 +130,4 @@ function App() {
         onClick={() => setMarkers(markers.slice(0, markers.length - 1))}>
         Remove marker
       </button>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+*/
--- src/markers.js
+++ src/markers.js
@@ -1,95 +1,127 @@
 
 const LondonMarkers = {
-    id: "LO",
-    markers: [
-      {
+  id: "UK",
+  markers: [
+    {
       id: 1,
       city: 'Singapore',
       color: 'red',
       coordinates: [1.3521, 103.8198],
       value: 50,
-      },
-      {
+    },
+    {
       id: 2,
       city: 'New York',
       color: 'blue',
       coordinates: [40.73061, -73.935242],
       value: 25,
-      },
-      {
+    },
+    {
       id: 3,
       city: 'San Francisco',
       color: 'orange',
       coordinates: [37.773972, -122.431297],
       value: 35,
-      },
-      {
+    },
+    {
       id: 4,
       city: 'Beijing',
       color: 'gold',
       coordinates: [39.9042, 116.4074],
       value: 0,
-      },
-      {
+    },
+    {
       id: 5,
       city: 'Milan',
       color: 'green',
       coordinates: [45.464664, 9.188540],
       value: 400,
-      },
-      {
+    },
+    {
       id: 6,
       city: 'Helsinki',
       color: 'green',
       coordinates: [60.192059, 24.94583],
       value: 80,
-      },
-      {
+    },
+    {
       id: 7,
       city: 'London',
       color: 'green',
       coordinates: [51.5074, 0.1278],
       value: 80,
-      }
-    ]
-  }
-
-const HelsinkiMarkers = {
-  id: "HKI",
-  markers: [
-    {
-      id: 4,
-      city: 'Random4',
-      color: 'green',
-      coordinates: [51.5074, 0.1278],
-      value: 400,
-    },
-    {
-      id: 1,
-      city: 'Random1',
-      color: 'green',
-      coordinates: [58.5074, 0.1278],
-      value: 80,
-    },
-    {
-      id: 2,
-      city: 'Random2',
-      color: 'green',
-      coordinates: [55.5074, 0.1278],
-      value: 500,
-    },
-    {
-      id: 3,
-      city: 'Random3',
-      color: 'green',
-      coordinates: [53.5074, 0.1278],
-      value: 80,
-    },
+    }
   ]
 }
 
+const AllMarkersByCountry = {
+  finland: {
+    markers: [
+      {
+        id: 4,
+        headline: 'Headline wow such wow',
+        color: 'green',
+        coordinates: [51.5074, 0.1278],
+        value: 400,
+      },
+      {
+        id: 1,
+        headline: 'Headline wow such wow',
+        color: 'green',
+        coordinates: [58.5074, 0.1278],
+        value: 80,
+      },
+    ]
+  },
+  uk: {
+    markers: [
+      {
+        id: 2,
+        headline1: 'Beef Consumption has increased by 35%',
+        headline2: 'Space Investigation In Trouble due to Infections',
+        color: 'red',
+        coordinates: [31.5074, -100],
+        value: 80,
+      },
+      {
+        id: 3,
+        headline1: 'Universities Supporting Healthcare Professionals ',
+        headline2: 'Tourism dropped by -120% in San Francisco',
+        headline3: 'Hollywood is in Financial Difficulties',
+        headline4: 'Silicon Valley`s tips for Remote Working',
+        headline5: 'California is Still considered safe zone',
+        color: 'red',
+        coordinates: [36, -119],
+        value: 80,
+      },
+      {
+        id: 4,
+        headline1: 'Universities Supporting Healthcare Professionals ',
+        headline2: 'Tourism dropped by -120% in San Francisco',
+        headline3: 'Hollywood is in Financial Difficulties',
+        headline4: 'Silicon Valley`s tips for Remote Working',
+        headline5: 'California is Still considered safe zone',
+        color: 'red',
+        coordinates: [28, -119],
+        value: 80,
+      },
+           {
+        id: 5,
+        headline1: 'Universities Supporting Healthcare Professionals ',
+        headline2: 'Tourism dropped by -120% in San Francisco',
+        headline3: 'Hollywood is in Financial Difficulties',
+        headline4: 'Silicon Valley`s tips for Remote Working',
+        headline5: 'California is Still considered safe zone',
+        color: 'red',
+        coordinates: [15, -95],
+        value: 80,
+      },
+    ]
+  }
+}
 
-export { 
-  LondonMarkers, 
-  HelsinkiMarkers
+
+export {
+  LondonMarkers,
+  AllMarkersByCountry
 }
